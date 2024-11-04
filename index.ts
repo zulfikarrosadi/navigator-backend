8fc45d9de68798fb1334b8cecb7e85b9fb47c0a9
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-11-04 10:37:23 +0700

    feat(routes): create POST /api/users endpoint

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import handler from './users/handler';
+import { validateInput } from './middlewares/validateInput';
+import { userCreateSchema } from './users/schema';
 
 const app = express();
 
 app.use(express.json());
+
+app.post('/api/users', validateInput(userCreateSchema), handler.createUser);
 
 app.listen(process.env.PORT, () =>
   console.log(`server running on port ${process.env.PORT}`),
