2aaa6035c411acf8162f9a9feb53536362036fe5
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-11-04 14:10:28 +0700

    feat: add validate user middleware

--- index.ts
+++ index.ts
@@ -4,12 +4,16 @@ import linkHandler from './links/handler'
 import { validateInput } from './middlewares/validateInput';
 import { userCreateSchema } from './users/schema';
 import { linksCreateSchema } from './links/schema';
+import validateUser from './middlewares/validateUser';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.post('/api/users', validateInput(userCreateSchema), userHandler.create);
+app.use(validateUser)
 app.post('/api/links', validateInput(linksCreateSchema), linkHandler.create);
 
 app.listen(process.env.PORT, () =>
