bcf546ed784a84ef157ffa9aa53deac1fa8ab20a
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-11-04 23:03:15 +0700

    feat(login): add login service

--- users/service.ts
+++ users/service.ts
@@ -1,8 +1,9 @@
 import userRepository from './repository';
 import type { ApiResponse } from '../schema';
 import type { UserCreateSchema } from './schema';
+import { AuthError } from '../error';
 
-const DEFAULT_BUN_HASH_COST = 4
+const DEFAULT_BUN_HASH_COST = 4;
 
 async function createUser(
   data: UserCreateSchema,
@@ -10,9 +11,12 @@ async function createUser(
   try {
     const hashedKey = Bun.password.hashSync(data.key, {
       algorithm: 'bcrypt',
-      cost: DEFAULT_BUN_HASH_COST
-    })
-    const newUser = await userRepository.createUser({...data, key: hashedKey});
+      cost: DEFAULT_BUN_HASH_COST,
+    });
+    const newUser = await userRepository.createUser({
+      ...data,
+      key: hashedKey,
+    });
 
     return {
       status: 'success',
@@ -36,6 +40,39 @@ async function createUser(
   }
 }
 
+async function login(
+  data: UserCreateSchema,
+): Promise<ApiResponse<{ id: number; username: string }>> {
+  try {
+    const user = await userRepository.getUserByUsername(data.username);
+    if (!user) {
+      throw new AuthError();
+    }
+    const isPasswordCorrect = Bun.password.verifySync(data.key, user.key);
+    if (!isPasswordCorrect) {
+      throw new AuthError();
+    }
+    return {
+      status: 'success',
+      data: {
+        users: {
+          id: user.id,
+          username: user.username,
+        },
+      },
+    };
+  } catch (error: any) {
+    return {
+      status: 'fail',
+      error: {
+        code: error.code,
+        message: error.message || error
+      }
+    };
+  }
+}
+
 export default {
   createUser,
+  login
 };
