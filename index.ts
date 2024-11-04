622ca8335928050ff73b35ac2f2b5918adc0d02e
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-11-04 11:45:21 +0700

    feat(link): add links resource endpoint

--- index.ts
+++ index.ts
@@ -1,13 +1,16 @@
 import express from 'express';
-import handler from './users/handler';
+import userHandler from './users/handler';
+import linkHandler from './links/handler'
 import { validateInput } from './middlewares/validateInput';
 import { userCreateSchema } from './users/schema';
+import { linksCreateSchema } from './links/schema';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/api/users', validateInput(userCreateSchema), handler.createUser);
+app.post('/api/users', validateInput(userCreateSchema), userHandler.create);
+app.post('/api/links', validateInput(linksCreateSchema), linkHandler.create);
 
 app.listen(process.env.PORT, () =>
   console.log(`server running on port ${process.env.PORT}`),
