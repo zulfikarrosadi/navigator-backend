a88760fc21eabb83ca3423a20056835718b971b1
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-11-05 14:07:44 +0700

    feat: add update link service

--- links/service.ts
+++ links/service.ts
@@ -1,6 +1,8 @@
-import { type LinkCreateSchema } from './schema';
+import { type LinkCreateSchema, type LinkUpdateSchema } from './schema';
 import repository from './repository';
 import type { ApiResponse } from '../schema';
+import { Prisma, type Link } from '@prisma/client';
+import { ServerError } from '../error';
 
 async function createLink(
   data: LinkCreateSchema,
@@ -23,4 +25,42 @@ async function createLink(
   }
 }
 
-export default { createLink };
+async function updateLink(
+  data: LinkUpdateSchema,
+  linkId: string,
+  userId: number,
+): Promise<ApiResponse<LinkUpdateSchema & { id: number }>> {
+  const parsedLinkId = parseInt(linkId);
+  if (Number.isNaN(parsedLinkId)) {
+    return {
+      status: 'fail',
+      error: {
+        code: 400,
+        message: 'fail to update link, link id not found',
+      },
+    };
+  }
+
+  try {
+    const updatedLink = await repository.updateLink(data, parsedLinkId, userId);
+    return {
+      status: 'success',
+      data: {
+        links: {
+          id: parsedLinkId,
+          title: updatedLink.title,
+          link: updatedLink.link,
+        },
+      },
+    };
+  } catch (error: any) {
+    return {
+      status: 'fail',
+      error: {
+        code: error.code,
+        message: error.message || error,
+      },
+    };
+  }
+}
+export default { createLink, updateLink, deleteLink, getLinks };
