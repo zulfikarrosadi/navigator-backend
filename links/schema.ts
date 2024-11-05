e8a4db8c166fdfc190176c5b6963abc02a29619c
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-11-05 14:14:52 +0700

    feat: add linkUpdateSchema validation rule

--- links/schema.ts
+++ links/schema.ts
@@ -11,3 +11,15 @@ export const linksCreateSchema = z.object({
 });
 
 export type LinkCreateSchema = z.infer<typeof linksCreateSchema>;
+
+export const linkUpdateSchema = z.object({
+  title: z
+    .string({ required_error: 'link title is required' })
+    .min(1, 'link title is required'),
+  link: z
+    .string({ required_error: 'link is required' })
+    .url('this is not valid url')
+    .min(1, 'link is required'),
+})
+
+export type LinkUpdateSchema = z.infer<typeof linkUpdateSchema>;
