14cd405f390ba2c7d0560cffb2897e4f2691e0a6
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-11-05 14:08:46 +0700

    chore: update create link handler
    update res locals type

--- links/handler.ts
+++ links/handler.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from 'express';
 import service from './service';
-import type { LinkCreateSchema } from './schema';
+import type { LinkCreateSchema, LinkUpdateSchema } from './schema';
 import type { ApiResponse, JWT_Payload } from '../schema';
 
 async function create(
@@ -9,16 +9,16 @@ async function create(
     Record<string, any>,
     LinkCreateSchema & { userId: number }
   >,
-  res: Response<ApiResponse<any>, {user: JWT_Payload}>,
+  res: Response<ApiResponse<any>, { user: JWT_Payload }>,
 ) {
   const result = await service.createLink(
     { link: req.body.link, title: req.body.title },
     res.locals.user.id,
   );
   if (result.status === 'fail') {
-    return res.status(result.error.code).json(result)
+    return res.status(result.error.code).json(result);
   }
-  return res.status(201).json(result)
+  return res.status(201).json(result);
 }
 
 export default { create };
